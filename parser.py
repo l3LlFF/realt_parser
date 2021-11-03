570b3aa043cb1289e21024c4803f7f11e76aa7d9
l3llff <boksha.va@energo-oil.by>
2021-11-03 09:13:26 +0300

    fixes

--- parser.py
+++ parser.py
@@ -6,16 +6,21 @@ import json
 from bs4 import BeautifulSoup
 import pandas as pd
 from decouple import config
+import aiofiles
+import os
+import random
 
 NUM_PAGES = int(config('NUM_PAGES', 2))
 NUM_SEMAPHORES = int(config('NUM_SEMAPHORES', 5))
 REALT_TYPE = config('REALT_TYPE', 'sale')
 REALT_OBJECT = config('REALT_OBJECT', 'offices')
+SVDIR = config('IMAGES_FOLDER', '/')
 
 
 durations = []
 hrefs = []
 result = []
+image_refs = []
 
 
 def scratch(content):
@@ -71,8 +76,9 @@ def scratch(content):
         agency = soup.find('div', {'class': 'agency-info-left'}).find('strong').text
     except:
         agency = ""
+    images = [x['data-src'] for x in soup.find_all('a', {'class': 'object-gallery-item'})]
     return [lat, lon, district, street, object_type, x_area, region, city,
-                 description, phone, price, price_per_meter, agency]
+                 description, phone, price, price_per_meter, agency], images
 
 
 def chunks(lst, n):
@@ -130,11 +136,41 @@ async def fetch_data(url, session, proxy):
     async with session.get(url, proxy=f"http://{host}:{port}",
                            proxy_auth=proxy_auth) as response:
         response_html = await response.text()
-    res = scratch(response_html)
+    res, images = scratch(response_html)
+
     if res[0] == '':
         result.append(None)
     else:
+        id = re.search(r'/([0-9]+)/', url).group(1)
+        res = [id] + res
+        image_refs.extend([(id, url) for url in images if 'realt' in url])
         result.append(res)
+
+
+async def fetch_image(url, session, proxy):
+    """
+    asynchronous get request
+    """
+    host, port, login, password = proxy
+    proxy_auth = aiohttp.BasicAuth(login, password)
+    print(url)
+    id, href = url
+
+    async with session.get(href, proxy=f"http://{host}:{port}",
+                           proxy_auth=proxy_auth) as response:
+        if response.status == 200:
+
+            if not os.path.exists(SVDIR):
+                os.makedirs(SVDIR)
+            folder = os.path.join(SVDIR, id)
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+            filename = f"{random.getrandbits(32)}.jpg"
+            sv_path = os.path.join(SVDIR, id, filename)
+            f = await aiofiles.open(sv_path, mode='wb')
+            await f.write(await response.read())
+            await f.close()
+
 
 
 async def gather_tasks(loop, urls, function, proxies, n_semaphores):
@@ -185,6 +221,10 @@ if __name__ == '__main__':
     async_run(urls=chunked_urls, function=fetch_data,
               proxies=proxies, n_semaphores=NUM_SEMAPHORES)
     result = clear_list(result)
-    pd.DataFrame(result, columns=['lat', 'lon', 'district', 'streets', 'object_type', 'area',
+    chunked_urls = chunks(image_refs, len(image_refs) // len(proxies))
+    async_run(urls=chunked_urls, function=fetch_image,
+              proxies=proxies, n_semaphores=NUM_SEMAPHORES)
+
+    pd.DataFrame(result, columns=['id', 'lat', 'lon', 'district', 'streets', 'object_type', 'area',
                                      'region', 'city', 'description', 'phone', 'price', 'prices_per_meter',
-                                     'agency']).to_excel('result.xlsx')
+                                     'agency']).to_excel(f'result_{REALT_TYPE}.xlsx', index=False)
